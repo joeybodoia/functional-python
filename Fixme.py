0bb3276ae2df8ebcafd250afa107299e1e0816cb
Joey Bodoia <jbodoia21@cmc.edu>
2021-02-07 18:15:27 -0800

    altered threes function to work with all numbers outside of test cases

--- Fixme.py
+++ Fixme.py
@@ -41,7 +41,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    return [num for num in range(0, n+1) if num % 10 == 3 or num // 10 == 3]
+    return list(filter(lambda x: '3' in str(x), range(0, n+1)))
 
 
 def small_words(text):
